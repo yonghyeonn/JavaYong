6a700ca492f51bfda7f912dec7863039d64b5634
woohyeon-dev <dngus3219@gmail.com>
2022-09-07 18:33:48 +0900

    first commit

---
+++ my_project/src/my_project/cheiru.java
@@ -0,0 +1,13 @@
+package my_project;
+
+public class cheiru {
+	public static void main(String[] args) {
+		
+		int firstValue  = 1; // Ã¹¹ø Â°
+		int secondValue = 2;
+		
+		int result = firstValue + secondValue;
+		System.out.println(result);
+		
+	}
+}
